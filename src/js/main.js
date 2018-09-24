a97af949f1952b12cd8823c7fa1dc8c612de4fb9
praveen-me <praveenkrsaini0@gmail.com>
2018-09-24 17:33:35 +0530

    Make a class coin

--- src/js/main.js
+++ src/js/main.js
@@ -93,3 +93,23 @@ class Lava {
 }
 
 Lava.prototype.size = new Vec(1, 1);
+
+// class for coin
+class Coin {
+  constructor(pos, basePos, wobble) {
+    this.pos = pos;
+    this.basePos = basePos;
+    this.wobble = wobble;
+  }
+
+  get type() {
+    return 'coin';
+  }
+
+  static create(pos) {
+    let basePos = pos.plus(new Vec(0.2, 0.1));
+    return new Coin(basePos, basePos, Math.random() * Math.PI() * 2);
+  }
+}
+
+Coin.prototype.size = new Vec(0.6, 0.6);
