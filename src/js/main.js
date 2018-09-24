c6026304c34f9eb2a8f7b1db918d03fe3d6713e7
praveen-me <praveenkrsaini0@gmail.com>
2018-09-24 17:19:15 +0530

    Add lava class and define different lava behaviour

--- src/js/main.js
+++ src/js/main.js
@@ -60,11 +60,36 @@ class Player {
 
   get type() {
     return "player";
+    
   }
-
   static create(pos) {
     return new Player(pos.plus(new Vec(0, -0.5)), new Vec(0,0));
   }
 }
 
 Player.prototype.size = new Vec(0.8, 1.5);
+
+// make a class of Lava
+class Lava {
+  constructor(pos, speed, reset) {
+    this.pos = pos;
+    this.speed = speed;
+    this.reset = reset;
+  }
+
+  get type() {
+    return "lava";
+  }
+
+  static create(pos, ch) {
+    if(ch === '=') {
+      return new Lava(pos, new Vec(2,0));
+    } else if(ch === '|') {
+      return new Lava(pos, new Vec(0, 2));
+    } else if(ch === 'v') {
+      return new Lava(pos, new Vec(0,3), pos)
+    }
+  }
+}
+
+Lava.prototype.size = new Vec(1, 1);
