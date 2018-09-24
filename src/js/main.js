b016860803a3d58da5c1a23965f60341a84a9471
praveen-me <praveenkrsaini0@gmail.com>
2018-09-24 16:28:46 +0530

    Make  a player class

--- src/js/main.js
+++ src/js/main.js
@@ -51,4 +51,20 @@ class Vec {
   }
 }
 
+// make a Player class for making our player
+class Player {
+  constructor(pos, speed) {
+    this.pos = pos;
+    this.speed = speed;
+  }
 
+  get type() {
+    return "player";
+  }
+
+  static create(pos) {
+    return new Player(pos.plus(new Vec(0, -0.5)), new Vec(0,0));
+  }
+}
+
+Player.prototype.size = new Vec(0.8, 1.5);
